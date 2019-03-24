501fbd79ba72318cbe29dc631465341956e3e603
youngle37 <aa258163@gmail.com>
2019-03-25 04:19:31 +0800

    Fix bug: forget to install oh-my-zsh plugins

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,7 +1,34 @@
 DIR=$1
 OHMYZSH=$2
 
-test -d $OHMYZSH && exit 1
+if [ ! -d $OHMYZSH ]; then
+    echo 'Installing oh-my-zsh.'
+    git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git $OHMYZSH
+else
+    echo 'Skip installing oh-my-zsh.'
+fi
 
-# Download oh-my-zsh
-git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git $OHMYZSH
+# zsh-syntax-highlighting
+if [ ! -d $OHMYZSH/custom/plugins/zsh-syntax-highlighting ]; then
+    echo 'Installing oh-my-zsh plugins (zsh-syntax-highlighting).'
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$OHMYZSH/custom}/plugins/zsh-syntax-highlighting
+else
+    echo 'Skip installing zsh-syntax-highlighting.'
+fi
+
+# zsh-autosuggestions
+if [ ! -d $OHMYZSH/custom/plugins/zsh-autosuggestions ]; then
+    echo 'Installing oh-my-zsh plugins (zsh-autosuggestions).'
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$OHMYZSH/custom}/plugins/zsh-autosuggestions
+else
+    echo 'Skip installing zsh-autosuggestions.'
+fi
+
+# zsh-completions
+if [ ! -d $OHMYZSH/custom/plugins/zsh-completions ]; then
+    echo 'Installing oh-my-zsh plugins (zsh-autosuggestions).'
+    git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-$OHMYZSH/custom}/plugins/zsh-completions
+else
+    echo 'Skip installing zsh-completions.'
+fi
+
